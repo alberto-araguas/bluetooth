88a5c648c0d50f774e07043bfb129918be9a0e88
alberto-araguas <alberto.araguas@gmail.com>
2017-08-11 16:56:24 +0200

    Add files via upload

---
+++ CONFIG_bLUEtOOTHhc_05/CONFIG_bLUEtOOTHhc_05.ino/CONFIG_bLUEtOOTHhc_05.ino.ino
@@ -0,0 +1,38 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(4, 5); // RX | TX
+// Connect the HC-05 TX to Arduino pin 2 RX. 
+// Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
+ 
+char c = ' ';
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("Arduino is ready");
+ 
+    // HC-05 default serial speed for communication mode is 9600
+    BTserial.begin(9600);  
+    Serial.println("BTserial started at 9600");
+}
+ 
+void loop()
+{
+    // Keep reading from HC-05 and send to Arduino Serial Monitor
+    if (BTserial.available())
+    {  
+        c = BTserial.read();
+        Serial.write(c);
+    }
+ 
+    // Keep reading from Arduino Serial Monitor and send to HC-05
+    if (Serial.available())
+    {
+        c =  Serial.read();
+ 
+        // Copy the serial data back to to the serial monitor. 
+        // This makes it easy to follow the commands and replies
+        Serial.write(c);
+        BTserial.write(c);  
+    }
+ 
+}
